7870d75531ba5047360906d295baf27cd16a43d9
Felipe Bojorquez <felipe@febog.com>
2021-10-08 19:38:59 -0700

    Generate emote JSON

--- index.js
+++ index.js
@@ -1,28 +1,69 @@
 require("dotenv").config();
 const axios = require("axios");
 
-let emoteList = [];
+class Emote {
+  /**
+   * For every emote, we collect the following data:
+   * @param {string} name Emote code
+   * @param {string} id Internal ID
+   * @param {string} img_url Image URL for thumbnail
+   * @param {string} page_url BTTV or FFZ emote page
+   * @param {string} type {FFZ, BTTV Channel, BTTV Shared}
+   */
+  constructor(name, id, img_url, page_url, type) {
+    // Constructor
+    this.name = name;
+    this.id = id;
+    this.img_url = img_url;
+    this.page_url = page_url;
+    this.type = type;
+  }
+}
 
-async function getEmotes() {
+var emoteList = [];
+
+async function updateEmoteList() {
   // Get FFZ channel emotes
   const ffzResponse = await axios.get(process.env.FFZ_CHANNEL_EMOTES_URL);
   ffzResponse.data.sets[process.env.FFZ_SET_ID].emoticons.map((e) =>
-    emoteList.push(e.name)
+    emoteList.push(getFfzEmote(e))
   );
-
-  // Get BTTV Global emotes, undocumented API.
-  const bttvGlobalResponse = await axios.get(
-    process.env.BTTV_GLOBAL_EMOTES_URL
-  );
-  bttvGlobalResponse.data.map((e) => emoteList.push(e.code));
 
   // Get BTTV channel and shared emotes, undocumented API.
   const bttvChannelResponse = await axios.get(
     process.env.BTTV_CHANNEL_EMOTES_URL
   );
-  bttvChannelResponse.data.channelEmotes.map((e) => emoteList.push(e.code));
-  bttvChannelResponse.data.sharedEmotes.map((e) => emoteList.push(e.code));
+  bttvChannelResponse.data.channelEmotes.map((e) =>
+    emoteList.push(getBttvEmote(e, "BTTV Channel"))
+  );
+  bttvChannelResponse.data.sharedEmotes.map((e) =>
+    emoteList.push(getBttvEmote(e, "BTTV Shared"))
+  );
   console.log("Number of emotes loaded: ", emoteList.length);
 }
 
-getEmotes();
+updateEmoteList();
+
+/**
+ * Parse BTTV response and return an emote
+ * @param {*} e Emote object from BTTV response
+ * @param {*} bttvType {BTTV Channel, BTTV Shared}
+ */
+function getBttvEmote(e, bttvType) {
+  let bttvEmoteUrl = `https://cdn.betterttv.net/emote/${e.id}/3x`;
+  let bttvEmotePage = `https://betterttv.com/emotes/${e.id}`;
+  let emote = new Emote(e.code, e.id, bttvEmoteUrl, bttvEmotePage, bttvType);
+  return emote;
+}
+
+/**
+ * Parse BTTV response and return an emote
+ * @param {*} e Emote object from FFZ response
+ */
+function getFfzEmote(e) {
+  // "4" URL is the highest resolution image
+  let ffzEmoteUrl = `https:${e.urls["4"]}`;
+  let ffzEmotePage = `https://www.frankerfacez.com/emoticon/${e.id}-${e.name}`;
+  let emote = new Emote(e.name, e.id, ffzEmoteUrl, ffzEmotePage, "FFZ");
+  return emote;
+}
